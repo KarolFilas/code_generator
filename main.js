27aa8f2b9a730d8f5b0cd28ffe36e4cf6056a54f
Karol Filas <filaskarol@gmail.com>
2023-04-08 19:01:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const chars = 'ABCDEFGIJK0123456789'
+
+const btn = document.querySelector('button')
+const section = document.querySelector('section')
+
+const codesNumber = 1000
+const charsNumber = 10
+
+const codesGenerator = () => {
+
+}
+
+btn.addEventListener('click', codesGenerator)
